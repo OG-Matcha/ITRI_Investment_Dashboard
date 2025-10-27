1e957049fd3aac1192f566aa22c511262c1dee78
黃鉅燊 <matchaskill@gmail.com>
2025-10-27 23:08:10 +0800

    style: add border to RiskQuadrant container for improved UI presentation

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,9 @@
                     </div>
 
                     <!-- 風險象限分析 -->
-                    <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+                    <div
+                        class="bg-white rounded-lg shadow-lg overflow-hidden border-2"
+                    >
                         <RiskQuadrant />
                     </div>
 
